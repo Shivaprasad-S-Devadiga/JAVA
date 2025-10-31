47ccfe61d0571eb27870719797f73f4f3d3488d1
Shivaprasad-S-Devadiga <Shivaprasada02@gmail.com>
2025-10-31 12:34:57 +0530

    Re-added _01Basics folder as normal tracked directory

---
+++ _01Basics/_01hello.java
@@ -0,0 +1,7 @@
+package _01Basics;
+
+public class _01hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ _01Basics/_02varibles.java
@@ -0,0 +1,17 @@
+package _01Basics;
+
+public class _02varibles{
+   public static void main(String[] args) {
+      int a=10, b=5;
+      String name = "Shivaprasad";
+      float f = 5.6f;
+      double d = 5.6;
+      System.out.println(a);
+      System.out.println("B IS "+b);
+      System.out.println("Name IS "+name);
+
+
+      
+   }
+
+}
